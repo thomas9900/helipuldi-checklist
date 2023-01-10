08f963c33df318e464eebae8c63b073d6059f10e
thomas9900 <52283912+thomas9900@users.noreply.github.com>
2023-01-10 16:39:40 +0200

    Update script.js

--- helipuldi kontrollnimekiri/script.js
+++ helipuldi kontrollnimekiri/script.js
@@ -29,21 +29,26 @@ if (setupTime == null) {
         const storedValues = window.localStorage.myitems;
         if (!storedValues) {
             list.innerHTML = 
-            '<li>Ülekanne (ülekande ajal peaks kogu aeg midagi kostma)</li>'+
-            '<li>Ventilatsioon</li>' +
-            '<li>Taustamuusika</li>'+
             '<li>Väljaanne alla laetud, videod (jw library)</li>'+
+            '<li>Taustamuusika</li>'+
             '<li>Helipuldi nupud õiged</li>' +
             '<li>Lavamikrofonide asetus kontrollida</li>' +
-            '<li>4 telekat</li>' +
+            '<li>3 telekat</li>' +
             '<li>Raadiomikrite saatja ja heliparandi sisse</li>' +
             '<li>Kontrolli taimerit (restardi taimeri kontrolleri kapis, kui ei tööta)</li>' +
+            '<li>Ventilatsioon</li>' +
             '<li>Taustamuusika kinni 2min enne algust</li>' +
+            '<h2>Zoom</h2>' +
+            '<li>Teenindaja määrata co-hostiks</li>' +
+            '<li>Kell :56 share video sisse</li>' +
+            '<li>Kell :58 share video välja</li>' +
+            '<li>Pilt ja heli sisse</li>' +
+            '<li>Spotlight</li>' +
             '<h2>Koosoleku alguses </h2>' +
-            '<li>Salvestamine</li>' +
+            '<li>Mikrofonid sisse</li>' +
             '<li>Laul käima, JW Library > Koosolek > Meedia</li>' +
             '<h2 class="h2-bottom">Koosoleku lõpus</h2>' +
-            '<li>Salvestamine lõpetada</li>' +
+            '<li>Zoom: spotlight, pilt, heli kinni</li>' +
             '<li>Taustamuusika</li>' +
             '<li>USB pulgad laetud</li>' +
             '<li>Telekad välja</li>';   
